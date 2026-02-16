61006bce1fadb94d7481f993fb21ff0b6dd75c96
bischoffisaias07-arch <bischoffisaias07@gmail.com>
2026-02-16 12:29:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -139,3 +139,7 @@ def modificar_producto(codigo: str, nueva_fecha: str):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
